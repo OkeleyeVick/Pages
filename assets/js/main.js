358e698efe71d70615de673443fe753506490704
Vickkk <justusofnigeria@gmail.com>
2023-03-05 01:40:07 +0100

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,17 +38,3 @@ function handleDropdownToggler(e) {
 	e.preventDefault();
 	dropdown.classList.toggle("active");
 }
-
-// user js controls
-const options = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a",
-		"X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
-	},
-};
-
-fetch("https://alpha-vantage.p.rapidapi.com/query?to_currency=JPY&function=CURRENCY_EXCHANGE_RATE&from_currency=USD", options)
-	.then((response) => response.json())
-	.then((response) => console.log(response))
-	.catch((err) => console.error(err));
